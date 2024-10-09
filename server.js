3c06b266c7c5629e413fdb69dfc0557d4a3577e2
Blake <bpatin77@gmail.com>
2024-10-08 22:53:22 -0500

    Added 404 handler along with its error-handling middleware

--- server.js
+++ server.js
@@ -27,6 +27,17 @@ app.get("/employees/:id", (req, res) => {
   }
 });
 
+// 404
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found" });
+});
+
+// Default error handler
+app.use((err, req, res, next) => {
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry, something went wrong!");
+});
+
 app.listen(PORT, () => {
   `Listening on port ${PORT}...`;
 });
